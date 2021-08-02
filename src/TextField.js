4f5bb8c3ae51c913e7e1cdf1e79491c121d22635
Florin <florin.popa1397@yahoo.ro>
2021-08-02 17:38:04 +0300

    fixed some issues

--- src/TextField.js
+++ src/TextField.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { ErrorMessage, useField } from 'formik';
+import React from "react";
+import { ErrorMessage, useField } from "formik";
 import "./home.css";
 
 const TextField = ({ label, onChange, ...props }) => {
-    const [field, meta] = useField(props);
-    return (
-        <div className={`field ${meta.touched && meta.error && "error"}`}>
-            <label htmlFor={field.name}>{label}</label>
-            <input autoComplete="off"
-                onChange={onChange}
-                {...field}
-                {...props} />
-            <ErrorMessage component="div" name={field.name} className="err" />
-        </div>
-    );
+  const [field, meta] = useField(props);
+  return (
+    <div className={`field ${meta.touched && meta.error && "error"}`}>
+      <label htmlFor={field.name}>{label}</label>
+      <input autoComplete="off" onChange={onChange} {...field} {...props} />
+      <ErrorMessage component="div" name={field.name} className="err" />
+    </div>
+  );
 };
 
 export default TextField;
